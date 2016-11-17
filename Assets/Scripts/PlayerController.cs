1118f3dd53a95f084222a8cce56e528611b3c4ce
t7pijo00 <t7pijo00@students.oamk.fi>
2016-11-17 17:36:19 +0200

    Second commit, finish
    finished tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
+		winText.text = "";
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
 	}
 	void FixedUpdate ()
     {
@@ -20,7 +27,16 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
     }
-
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick up"));
+			other.gameObject.SetActive(false);
+		count = count + 1;
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 4) {
+			winText.text = "You WIN!!!!";
+		}
+			
+	}
 
       
 }
